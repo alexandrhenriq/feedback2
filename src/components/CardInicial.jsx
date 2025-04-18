38adb63bcb5ae1334ed1082a614aaf0aa122020a
alexandrehenriquegomeslopes <alexandrehenriquegomeslopes@gmail.com>
2025-04-18 20:55:10 +0000

    criação do componente para o card final e ajuste do transition dos botões + correcão de erros do meu código

--- src/components/CardInicial.jsx
+++ src/components/CardInicial.jsx
@@ -22,14 +22,14 @@ export function CardInicial({ setNotaAvaliacao, notaAvaliacao, setSubmited }) {
         <p className="text-light-grey text-sm leading-1 mb-6">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
     
         <div className="flex justify-between mb-6">
-          <InputButton value={1} handleAlterarNotaAvaliacao={handleAlterarNotaAvaliacao} notaAvaliacao={notaAvaliacao} setNotaAvaliacao={setNotaAvaliacao}/>
-          <InputButton value={2} handleAlterarNotaAvaliacao={handleAlterarNotaAvaliacao} notaAvaliacao={notaAvaliacao} setNotaAvaliacao={setNotaAvaliacao}/>  
-          <InputButton value={3} handleAlterarNotaAvaliacao={handleAlterarNotaAvaliacao} notaAvaliacao={notaAvaliacao} setNotaAvaliacao={setNotaAvaliacao}/>
-          <InputButton value={4} handleAlterarNotaAvaliacao={handleAlterarNotaAvaliacao} notaAvaliacao={notaAvaliacao} setNotaAvaliacao={setNotaAvaliacao}/>
-          <InputButton value={5} handleAlterarNotaAvaliacao={handleAlterarNotaAvaliacao} notaAvaliacao={notaAvaliacao} setNotaAvaliacao={setNotaAvaliacao}/>
+          <InputButton value={1} setNotaAvaliacao={setNotaAvaliacao}/>
+          <InputButton value={2} setNotaAvaliacao={setNotaAvaliacao}/>  
+          <InputButton value={3} setNotaAvaliacao={setNotaAvaliacao}/>
+          <InputButton value={4} setNotaAvaliacao={setNotaAvaliacao}/>
+          <InputButton value={5} setNotaAvaliacao={setNotaAvaliacao}/>
         </div>
     
-        <button onClick={handleSubmited} className="bg-orange w-full py-3 uppercase text-sm rounded-3xl tracking-1 font-bold hover:bg-white hover:text-orange hover:cursor-pointer">Submit</button>
+        <button onClick={handleSubmited} className="bg-orange w-full py-3 uppercase text-sm rounded-3xl tracking-1 font-bold hover:bg-white hover:text-orange hover:cursor-pointer duration-500">Submit</button>
       </div>
     )
 }
